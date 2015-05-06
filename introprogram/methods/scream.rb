52e61dbd198b30b70f75d4d56146e5fdb35d7180
flo <flo.liang@my.com>
2015-05-05 20:36:42 -0700

    methods solution 5

--- introprogram/methods/scream.rb
+++ introprogram/methods/scream.rb
@@ -1,7 +1,8 @@
 def scream(words)
   words = words + "!!!!"
-  return # will never excecute the action below
   puts words
 end
 
-scream("Yippeee") # => nothing
+scream("Yippeee") 
+
+# still returns nil in irb
