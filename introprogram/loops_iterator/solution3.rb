7307af8a2391debc9d51c36d62641faf84d23b30
floalex <floalex@github.com>
2015-05-07 18:00:11 +0000

    Complete loops_iterator exercises

---
+++ introprogram/loops_iterator/solution3.rb
@@ -0,0 +1,9 @@
+top_five_colors = ["Red",
+                   "Green",
+                   "Black",
+                   "White",
+                   "Purple"]
+                   
+top_five_colors.each_with_index do | color, index |
+  puts "#{index + 1}. #{game}"
+end
