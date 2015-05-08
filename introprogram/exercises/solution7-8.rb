7bb43ca03507703973deaf7d09bb12154038ff3a
floalex <floalex@github.com>
2015-05-08 23:52:37 +0000

    Complete Exercises

---
+++ introprogram/exercises/solution7-8.rb
@@ -0,0 +1,4 @@
+# 7 The major difference between an array and a hash is that a hash contains a key value pair for referencing by key.
+
+hash = {:name => 'bob'} # <= old version
+hash = {name: 'bob'} # <= new version
